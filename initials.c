63dc24326565cbc5306b199dae4c728adf236fd5
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-11-14 22:04:53 +0100

    INITIALS

--- initials.c
+++ initials.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - This code prints my initials
  * Return: always 0
  */
 
-int main(void)
+int main (void)
 {
+	printf("My initials are: %c.%c.%c \n", 'K', 'H', 'T');
 	
+	return(0);
+}
